2281be31be0604cbc4585165a7ac3fd267d12870
kite_n_code <stephen.e.novotny@gmail.com>
2020-07-28 11:28:03 +1000

    mage final grid layout

--- src/GridDemo.tsx
+++ src/GridDemo.tsx
@@ -15,7 +15,43 @@ export const GridBox = () => {
     return (
       <>
         <h4>Two Column Grid</h4>
+        <h5>Max 3 Cols of 300px</h5>
         <div className="two-col-grid">
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+          <div className="col">
+            <BuildCard />
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const MaxThreeColGrid = () => {
+    return (
+      <>
+        <h4>Max Three Column Grid</h4>
+        <h5>minmax(300px, 1fr)</h5>
+        <div className="max-three-col-grid">
           <div className="col">
             <BuildCard />
           </div>
@@ -47,9 +83,14 @@ export const GridBox = () => {
 
   return (
     <>
+      <MaxThreeColGrid />
+      <br />
+
       <TwoColGrid />
+
       <br />
       <h4>Grid</h4>
+      <h3>repeat(auto-fill, minmax(300px, 1fr)</h3>
       <div className="grid">
         <div className="col">
           <BuildCard />
