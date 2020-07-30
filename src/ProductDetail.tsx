4eb5c368e2092af58c8300c1e537ad615ab18f1e
kite_n_code <stephen.e.novotny@gmail.com>
2020-07-30 14:04:20 +1000

    adding product detail

--- src/ProductDetail.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-export const ProductDetail = () => {
-  return <div>Product Details</div>;
-};
