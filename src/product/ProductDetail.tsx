090e155173efd729c127f28424e31ceea1d3adc6
kite_n_code <stephen.e.novotny@gmail.com>
2020-07-31 15:21:31 +1000

    bug fix

--- src/product/ProductDetail.tsx
+++ src/product/ProductDetail.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Button, Dropdown } from "semantic-ui-react";
+import { Dropdown } from "semantic-ui-react";
 
 const friendOptions = [
   {
