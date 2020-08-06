f0355266e1107e9220a019396631deb95bcdc00e
kite_n_code <stephen.e.novotny@gmail.com>
2020-08-06 10:49:53 +1000

    Routes .menu header font smaller

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,45 @@ import * as React from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import { GridBox } from "./GridDemo";
 import { ProductDetail } from "./product/ProductDetail";
-import "./styles.css";
-import "./GridDemo";
-import "./navbar-styles.css";
-// import { GiHamburgerMenu } from "react-icons/gi";
 import { GrMenu } from "react-icons/gr";
 import { Box } from "@chakra-ui/core";
+import "./GridDemo";
+import "./navbar-styles.css";
+import "./styles.css";
+// import styled from "styled-components";
+// const color = "brown";
 
 export default function App() {
+  /*   const Demo = styled.div`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    color: ${props => (props.primary ? "white" : "palevioletred")};
+    border-radius: 4px;
+    &:hover {
+      color: ${color};
+    }
+  `; */
   return (
     <main>
       <div>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/product" exact component={GridBox} />
-          <Route path="/productdetail" exact component={ProductDetail} />
+          <Route path="/" exact component={GridBox} />
+          <Route path="/how" exact component={How} />
           <Route path="/about" exact component={About} />
+          <Route path="/faq" exact component={Faq} />
+          <Route path="/productdetail" exact component={ProductDetail} />
         </Switch>
       </div>
     </main>
   );
 }
 
-function Home() {
+function How() {
   return (
     <div>
-      <h2>Home Page</h2>
+      <h2>How to page</h2>
     </div>
   );
 }
@@ -40,6 +52,14 @@ function About() {
     </div>
   );
 }
+function Faq() {
+  return (
+    <div>
+      <h2>FAQ Page</h2>
+    </div>
+  );
+}
+
 function Navbar() {
   return (
     <div style={{ backgroundColor: "rgb(252,252,252)" }}>
@@ -52,15 +72,15 @@ function Navbar() {
         <div className="nav-container">
           <div className="logo-container">Logo</div>
           <div className="link-container">
-            <Link className="site-nav-link" to="/product">
+            <Link className="site-nav-link" to="/">
               shop now
             </Link>
 
-            <Link className="site-nav-link" to="/productdetail">
+            <Link className="site-nav-link" to="/how">
               how it works
             </Link>
 
-            <Link className="site-nav-link" to="/">
+            <Link className="site-nav-link" to="/faq">
               faq
             </Link>
 
@@ -68,7 +88,7 @@ function Navbar() {
               About Us
             </Link>
 
-            <Link className="site-nav-link" to="/">
+            <Link className="site-nav-link" to="/cart">
               cart
             </Link>
           </div>
--- src/product/ProductDetail.tsx
+++ src/product/ProductDetail.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import styled from "styled-components";
 import {
   Select,
-  // Stack,
   Input,
   Text,
   Badge,
